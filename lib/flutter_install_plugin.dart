3f62d9813c2ba251213602a633dda3ec12746919
dashu <bigtree2018102@gmail.com>
2021-12-01 10:57:13 +0700

    support null-safety;

--- lib/flutter_install_plugin.dart
+++ lib/flutter_install_plugin.dart
@@ -6,8 +6,8 @@ class FlutterInstallPlugin {
   static const MethodChannel _channel =
       const MethodChannel('flutter_install_plugin');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
@@ -15,7 +15,7 @@ class FlutterInstallPlugin {
   /// if the target platform is higher than android 24:
   /// a [appId] is required
   /// (the caller's applicationId which is defined in build.gradle)
-  static Future<String> installApk(String filePath, String appId) async {
+  static Future<String?> installApk(String filePath, String appId) async {
     Map<String, String> params = {'filePath': filePath, 'appId': appId};
     return await _channel.invokeMethod('installApk', params);
 //    if (Platform.isAndroid) {
